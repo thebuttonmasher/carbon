5c2c3d2b22a9edb5fda8a37feff8f63a2490b9fb
Tomer <tomer.hlprin22@gmail.com>
2023-01-06 01:57:33 +0200

    Add project files.

---
+++ carbon/Source.cpp
@@ -0,0 +1,19 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
+
+int command_loop()
+{
+	int command;
+	while (1)
+	{
+		
+	}
+}
+
+int main()
+{
+	std::cout << "hello world!";
+}
